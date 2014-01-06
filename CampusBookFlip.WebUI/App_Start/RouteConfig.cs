b84249a9d0293e16b11e646c3dc5a478f6ae18ec
Jimi Ford <jimifordmail@gmail.com>
2014-01-06 02:05:48 -0500

    Added dashed route for controllers and actions. participating colleges link works.

--- CampusBookFlip.WebUI/App_Start/RouteConfig.cs
+++ CampusBookFlip.WebUI/App_Start/RouteConfig.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using LowercaseDashedRouting;
 
 namespace CampusBookFlip.WebUI
 {
@@ -11,13 +12,18 @@ namespace CampusBookFlip.WebUI
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
+            
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
             routes.LowercaseUrls = true;
-            routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+            //routes.MapRoute(
+            //    name: "Default",
+            //    url: "{controller}/{action}/{id}",
+            //    defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            //);
+            routes.Add(new LowercaseDashedRoute(url: "{controller}/{action}/{id}",
+                defaults: new RouteValueDictionary(
+                new { controller = "Home", action = "Index", id = UrlParameter.Optional }),
+                routeHandler: new DashedRouteHandler()));
         }
     }
 }
---
+++ CampusBookFlip.WebUI/Controllers/ParticipatingCollegesController.cs
@@ -0,0 +1,28 @@
+﻿using CampusBookFlip.Domain.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CampusBookFlip.WebUI.Controllers
+{
+    public class ParticipatingCollegesController : Controller
+    {
+        private IRepository repo;
+
+        public ParticipatingCollegesController(IRepository repo)
+        {
+            this.repo = repo;
+        }
+        //
+        // GET: /ParticipatingColleges/
+
+        public ActionResult Index()
+        {
+
+            return View(repo.Institution.Where(i => i.Activated).OrderBy(c => c.State).ThenBy(c => c.City).ThenBy(c => c.Name));
+        }
+
+    }
+}
