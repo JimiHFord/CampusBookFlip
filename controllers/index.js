1a089996fdbacb7abb5ec1eaceed23316cb1f28a
Jimi Ford <JimiHFord@users.noreply.github.com>
2015-10-03 15:21:51 -0400

    add html page with domains of colleges

--- controllers/index.js
+++ controllers/index.js
@@ -1,4 +1,4 @@
- var express = require('express');
+var express = require('express');
 var router = express.Router();
 var users = require('./users');
 var facebook = require('../config/facebook');
@@ -7,8 +7,7 @@ var isAuthenticated = function (req, res, next) {
 	// if user is authenticated in the session, call the next() to call the next request handler
 	// Passport adds this method to request object. A middleware is allowed to add properties to
 	// request and response objects
-	if (req.isAuthenticated())
-		return next();
+	if (req.isAuthenticated()) { return next(); }
 	// if the user is not authenticated then redirect him to the login page
 	res.redirect('/');
 }
