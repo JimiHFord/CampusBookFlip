e2136fe5789b518d5b8f41da223b6949f1c16635
Jimi Ford <JimiHFord@users.noreply.github.com>
2015-10-06 00:40:57 -0400

    connect to redis. make script that adds institution_action = true to all colleges

--- config/session.js
+++ config/session.js
@@ -1,14 +1,13 @@
 module.exports = function(options) {
-  var session = options.session,
-      mongoose = options.mongoose;
-  var MongoStore = require('connect-mongo')(session);
+  var RedisStore = require('connect-redis')(options.session);
   return {
     secret: 'jv93n8x9wp9h3h98hqllwekno09j2nds302v8hzq9eg7stwhx54f2mv2bgd02j',
     name: 'com.campusbookflip',
     saveUninitialized: true,
     resave: false,
-    store: new MongoStore({
-      mongooseConnection: mongoose.connection
-    })
+    store: new RedisStore({
+      host: options.host,
+      port: options.port
+    }),
   };
 };
