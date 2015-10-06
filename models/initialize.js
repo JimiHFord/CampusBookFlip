5b201287a3d74c6b1d2df032d9ce93be6e810548
Jimi Ford <jimifordmail@gmail.com>
2015-10-05 22:13:34 -0400

    made model initialization more robust

--- models/initialize.js
+++ models/initialize.js
@@ -1,7 +1,13 @@
-var models = ['Book', 'College', 'User'];
+var fs = require('fs');
+
+var _ran = false;
 
 module.exports = function() {
-  for(var i = 0; i < models.length; i++) {
-    require('./'+models[i])();
+  if(!_ran) {
+    var models = fs.readdirSync(__dirname);
+    for(var i = 0; i < models.length; i++) {
+      models[i] !== 'initialize.js' && require('./'+models[i])();
+    }
   }
+  return _ran = true;
 };
